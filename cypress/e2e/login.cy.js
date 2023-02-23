1d469e693c3b55cf39a87e66479972439754b402
fabianofda <54209603+fabianofda@users.noreply.github.com>
2023-02-23 18:46:19 -0300

    Update login.cy.js
    arrumando a falha no script dos testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,12 +23,12 @@ describe('Login', () => {
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciai inválidas :(')
+            .should('have.text', 'Oops! Credenciais inválidas :(')
     })
 
     it('com sucesso', () => {
         cy.login('qa', 'xperience')
         cy.get(modalContent)
-            .should('have.text', 'Sua credenciais são válidas :)')
+            .should('have.text', 'Suas credenciais são válidas :)')
     })
 })

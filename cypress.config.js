0deb4710b3e6ea31df1393ddc67dba68e8647c79
João Lucas Fernandes <joaolucasfernandes@gmail.com>
2023-03-12 10:39:33 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,15 @@ module.exports = defineConfig({
   projectId: 'r2b7k3',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+
+        if (browser.family === 'firefox') {
+          // auto open devtools
+          launchOptions.args.push('--disable-gpu')
+        }
+
+        return launchOptions
+      })
     },
     viewportWidth: 1280,
     viewportHeight: 720,
